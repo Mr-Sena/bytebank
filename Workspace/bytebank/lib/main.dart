7ff31c102f4e51f8156451137a289314c4108481
Mr-Sena <jsenna985@gmail.com>
2021-08-29 18:22:37 -0300

    Estado do App: Layout básico com uma lista de valores simulando transfêrencias.

--- Workspace/bytebank/lib/main.dart
+++ Workspace/bytebank/lib/main.dart
@@ -2,6 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: Column(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('110.00'),
+                subtitle: Text('1000'),
+              )
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('100.00'),
+                subtitle: Text('1000'),
+              )
+            ),
+          ],
+        ),
         appBar: AppBar(title: Text('Transferências'),), //Barra do app - Extende uma faixa horizontal na parte superior, Com o nome 'Transferências'
         floatingActionButton: FloatingActionButton( // Botão flutuante. Icone do botão definida no Child, pela Icon (que recebe o icone definitivamente)
           onPressed: () {  },
