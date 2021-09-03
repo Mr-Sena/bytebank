2be7a9ea1626f425786b9e721cb20d42e856e6cd
Mr-Sena <jsenna985@gmail.com>
2021-09-02 23:09:35 -0300

    Refatorção permissível de Hot Reload
    Através da extração dos código para classes isoladas - o que promove uma
    maior manutenibilidade e legibilidade ao código.

--- Workspace/bytebank/lib/main.dart
+++ Workspace/bytebank/lib/main.dart
@@ -1,29 +1,87 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('110.00'),
-                subtitle: Text('1000'),
-              )
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.00'),
-                subtitle: Text('1000'),
-              )
-            ),
-          ],
-        ),
-        appBar: AppBar(title: Text('Transferências'),), //Barra do app - Extende uma faixa horizontal na parte superior, Com o nome 'Transferências'
-        floatingActionButton: FloatingActionButton( // Botão flutuante. Icone do botão definida no Child, pela Icon (que recebe o icone definitivamente)
-          onPressed: () {  },
-          child: Icon(Icons.add),
-        ),
+        body: FormularioTransferencias(),
       ),
-    ));
+    );
+  }
+}
+
+
+
+class FormularioTransferencias extends StatelessWidget {
+  const FormularioTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Efetuando Transferências'),),
+    );
+  }
+}
+
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Transferências'),), //Barra do app - Extende uma faixa horizontal na parte superior, Com o nome 'Transferências'
+      body: Column(
+        children: <Widget>[
+
+          ItemTransferencia(Transferencia(37.00, 5537)),
+          ItemTransferencia(Transferencia(57.00, 7542)),
+          ItemTransferencia(Transferencia(34.00, 7321)),
+
+
+
+        ],
+      ),
+
+      floatingActionButton: FloatingActionButton( // Botão flutuante. Icone do botão definida no Child, pela Icon (que recebe o icone definitivamente)
+        onPressed: () {  },
+        child: Icon(Icons.add),
+      ),
+
+    );
+  }
+
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(this._transferencia.valor.toString()),
+          subtitle: Text(this._transferencia.numeroConta.toString()),
+        )
+    );
+
+  }
+
+}
+
+class Transferencia {
+
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+
+}
